4e8fb667cacb9b8176ddd25320ab5709338109d7
charleyferrari <charley@charleyferrari.com>
2014-10-06 09:46:54 -0400

    Adding Week 6 Assignment

--- IS 607 Data Acquisition and Management/Week 6/Assignment/Week6Assignment.R
+++ IS 607 Data Acquisition and Management/Week 6/Assignment/Week6Assignment.R
@@ -1,5 +1,13 @@
 library(rvest)
 
+lego_movie <- html("http://www.imdb.com/title/tt1490017/")
+
+rating <- lego_movie %>% 
+  html_nodes("strong span") %>%
+  html_text() %>%
+  as.numeric()
+rating
+
 theURL <-"http://en.wikipedia.org/wiki/List_of_paradoxes"
 
 theSource <- html(theURL)
@@ -13,6 +21,8 @@ theSource <- html(theURL)
 #  html_nodes(".column-count-2 ul li a") %>%
 #  html_text()
 #cocktailList <- cocktailList[cocktailList != "IBA"]
+
+#cocktailList
 
 paradoxList <- theSource %>%
   html_nodes("p+ ul b , dd b , .thumb+ ul b , h3+ ul b , .seealso+ ul a b , h2+ ul a:nth-child(1) b") %>%
