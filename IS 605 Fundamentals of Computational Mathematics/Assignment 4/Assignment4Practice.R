ac22c15e0a48e52d658a6c7c55399615e13dbdba
charleyferrari <charley@charleyferrari.com>
2015-02-20 17:49:24 -0500

    Adding IS 605 Assignment 4

---
+++ IS 605 Fundamentals of Computational Mathematics/Assignment 4/Assignment4Practice.R
@@ -0,0 +1,21 @@
+
+
+A <- matrix(c(1,-1,2,0,3,4),nrow=2)
+A
+
+
+AtA <- t(A) %*% A
+
+
+Y
+eigen(X)
+
+eigen(Y)
+
+a <- 1
+b <- -10
+c <- 16
+
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+eigen(matrix(c(5,3,3,5),nrow=2))
--- IS 608 Knowledge and Visual Analytics/Assignment 2/Assignment 2.R
+++ IS 608 Knowledge and Visual Analytics/Assignment 2/Assignment 2.R
@@ -1,7 +1,9 @@
 install.packages("devtools")
 install.packages("Rtools")
-devtools::install_github("bigvis")
+devtools::install_github("hadley/bigvis")
+install.packages("bigVis")
 library(Rtools)
+library(installr)
 
 install.packages("Rcpp", type="source")
 
@@ -9,7 +11,17 @@ install.packages("installr")
 ?devtools::install_github
 library(bigvis)
 
+devtools::find_rtools()
+
+Sys.getenv("PATH")
+system('g++ -v')
+system('where make')
+
 
 setwd("E:/CUNY Semester 1/IS 607 Data Acquisition and Management/Semester Project")
 
 weekends <- read.csv("weekends.csv")
+library(bigvis)
+
+remove.packages("Rcpp")
+install.packages("C:/Users/Charley/Downloads/Rcpp_0.11.3.tar.gz", repos=NULL, type="source")
